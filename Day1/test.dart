68c48b180ead4e8225aead4024818b9ccdae0775
HkbCodes <harshilj2002@gmail.com>
2024-09-01 23:39:14 +0530

    Day-1

---
+++ Day1/test.dart
@@ -0,0 +1,13 @@
+void main() {
+  String firstApp = "Hello WORLD!";
+  print(firstApp);
+  double age = 22.0;
+  print(age);
+  int year = 2002;
+  print(year);
+  bool male = true;
+  print(male);
+  if (male) {
+    print("Male");
+  }
+}
